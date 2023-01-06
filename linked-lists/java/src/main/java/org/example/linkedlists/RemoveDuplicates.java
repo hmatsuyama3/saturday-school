42c5e82c4bdb8ad00eab37fb026de50ed7c472cf
Hiroko Matsuyama <110796421+hmatsuyama3@users.noreply.github.com>
2023-01-05 22:09:34 -0500

    Remove Duplicates
    Started working - learned that LinkedListNode is NOT a class in java lol. pushing and will retry in VS Code...

--- linked-lists/java/src/main/java/org/example/linkedlists/RemoveDuplicates.java
+++ linked-lists/java/src/main/java/org/example/linkedlists/RemoveDuplicates.java
@@ -1,8 +1,18 @@
 package org.example.linkedlists;
 
 
+import java.util.HashSet;
+
 public class RemoveDuplicates {
 
     public static void remove(LinkedListNode<Integer> node) {
+        HashSet<Integer> set = new HashSet<>();
+        LinkedListNode<Integer> previous = null;
+        while(node != null){
+            if(set.contains(node.value)){
+
+            }
+        }
+
     }
 }
