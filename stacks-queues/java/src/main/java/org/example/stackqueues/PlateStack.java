dc7d70df0f13d1d8ed882debf317eb463809d0b1
Hiroko Matsuyama <110796421+hmatsuyama3@users.noreply.github.com>
2023-01-27 22:50:35 -0500

    Update PlateStack.java

--- stacks-queues/java/src/main/java/org/example/stackqueues/PlateStack.java
+++ stacks-queues/java/src/main/java/org/example/stackqueues/PlateStack.java
@@ -2,15 +2,38 @@ package org.example.stackqueues;
 
 public class PlateStack {
 
+    private Object[] memory;
+    private int stacksize;
+    private PlateStack stack;
+
     public PlateStack(int capacity) {
+
+        stack = new Object[capacity];
+        stacksize = capacity;
+        memory.add(stack); //add the stack to the memory array
+
 
     }
 
     public Object[] getBackingArrays() {
-        return null;
+        return memory;
     }
 
     public void push(int value) {
+        //need to place a check to see how many items are coming in
+        //if over the stacksize capacity, start a new stack
+
+        int counter = 0;
+        if(counter<stacksize){
+            stack.push(value); //add value to top of stackd
+            counter++;
+        }
+        else if(counter==stacksize){
+            stack = new Object[stacksize]; //not sure how to make new space in memory?
+            stack.push(value); //add value to new stack
+            memory.add(stack); //add stack to the memory array
+            counter = 0; //reset counter
+        }
 
     }
 
@@ -19,7 +42,21 @@ public class PlateStack {
     }
 
     public Object pop() {
-        return null;
+        let index = memory.length-1;
+        let isEmpty=true;
+        while(isEmpty){ //look for first stack that is not empty
+            if(memory[index].isEmpty()){
+                index-=1;
+                if(memory[index].isEmpty()){ //if current & next index are empty
+                    memory.remove(index+1);  //remove the last element
+                }
+            }else{
+                isEmpty=false;
+            }
+        }
+
+        return memory[index].pop(); //pop from the last element in the memory array that isn't empty
+
     }
 
     private void decreaseCapacity() {
@@ -27,11 +64,26 @@ public class PlateStack {
     }
 
     public int search(Object object) {
+        let stacknum = memory.length-1;
+        let counter = stacksize;
+        for(int i=0; i<stacksize; i++){
+            if(memory[stacknum].getStack(counter-1) == object){ 
+                return i;
+            }else{
+                counter--;
+            }
+
+            if(counter == 0){
+                stacknum--; //move to next stack
+                counter = stacksize; //reset counter
+            }
+        }
+
         return -1;
     }
 
     public Object peek() {
-        return null;
+        return memory[memory.length-1].peek(); //peek at the last stack
     }
 
     public Object popAt(int position) {
